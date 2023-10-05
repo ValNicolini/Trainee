e03d1d646d744ea1defe58f3e642066d40ed9193
ValNicolini <val_nicolini13@hotmail.com>
2023-10-05 16:43:14 -0300

    Empresa

--- Treinamento.py
+++ Treinamento.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 
 def homepage():
-    return 'Teste + um'
+    return 'Teste'
+
+@app.route('/contatos')
+
+def contatos():
+   return '<h1>Contatos</h1>'
 
 if __name__ == '__main__':
  app.run(debug=True)
