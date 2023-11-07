a8629ccf0ad0780c009594e5641c3d2644f21544
ValNicolini <val_nicolini13@hotmail.com>
2023-11-07 14:59:32 -0300

    Empresa

--- Treino.py
+++ Treino.py
@@ -23,35 +23,36 @@
 # AULA 57
 # from openpyxl import load_workbook
 #
-# arquivo = 'C:\\Users\\silva.valdenir\\Downloads\\nomes.xlsx'
-# wb = load_workbook(arquivo)
+#
+# wb = load_workbook('nomes.xlsx')
 #
 # planilha = wb['Planilha1']
-
+#
 # for i in range(2,5):
 #     print('%s tem %s anos.' % (planilha['A%s' % i].value, planilha['B%s' % i].value))
 
 # AULA 59
 from openpyxl import Workbook
 
-print('Iniciando o robô...')
-print('Lendo dados do arquivo de texto...')
-file = open('gastos.txt', 'r', encoding='utf-8' )
+# print('Iniciando o robô...')
+# print('Lendo dados do arquivo de texto...')
+# with open('gastos.txt', 'r', encoding='utf-8' ) as file:
+#
+# # lê o arquivo
+#  arquivo = file.read()
+#
+# lista = arquivo.splitlines()
+#
+# for i in range(0, len(lista)):
+#     lista[i] = lista[i].split(',')
+#
 
-# lê o arquivo
-arquivo = file.read()
-
-lista = arquivo.splitlines()
-
-for i in range(0, len(lista)):
-    lista[i] = lista[i].split(',')
-
-# Criando arquivo
-print('criando arquivo excel...')
-wb = Workbook()
-ws = wb.active
-
-for row in lista:
-    ws.append(row)
-
-wb.save('gastos.xlsx')
+# # Criando arquivo
+# print('criando arquivo excel...')
+# wb = Workbook()
+# ws = wb.active
+#
+# for row in lista:
+#     ws.append(row)
+#
+# wb.save('gastos.xlsx')
